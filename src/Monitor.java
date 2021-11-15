29e26253740ca8d21ab9c1fbb72a566996224647
Hyperion <mire.maubanc@gmail.com>
2021-11-15 09:04:20 +0100

    Add monitor class + attributes

---
+++ src/Monitor.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.time.LocalDateTime;
+
+public class Monitor {
+    private List<String> OpenStatus;
+    private int Level;
+    private List<String> Metric;
+    private LocalDateTime LastCoffee;
+    private int NumberCoffee;
+    private float TempCoffee;
+    private List<String> Errors;
+
+    
+}
