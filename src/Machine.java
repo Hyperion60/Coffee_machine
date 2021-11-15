4b5497f8d7c018f3720c665945c8b39a8cde653c
ouyed_y <ya.ouyed@gmail.com>
2021-11-15 09:06:05 +0100

    Add Machine attributes

--- src/Machine.java
+++ src/Machine.java
@@ -1,2 +1,9 @@
+import java.util.List;
+
 public class Machine {
+    private String norme;
+    private int capacity;
+    private List<String> type;
+    private String location;
+    private Monitor monitor;
 }
