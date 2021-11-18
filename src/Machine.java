93c628dfdd614dfe8ef07e9e1d8fe01e9754e4e8
ouyed_y <ya.ouyed@gmail.com>
2021-11-18 11:13:43 +0100

    Add Machine methods

--- src/Machine.java
+++ src/Machine.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Machine {
-    //declaration
+    //1.declaration
     private String Norme;
     private int Capacity;
     private List<String> Type;
     private String Location;
     private Monitor Monitor;
-    //initialisation
+
+
+    //2.initialisation
     public Machine()
     {
         this.Norme = "Vendor description";
@@ -30,22 +32,42 @@ public class Machine {
     }
 
     //Capacity
-    public int get_string_capacity(){
+    public int get_string_Capacity(){
         return this.Capacity;
     }
-    public boolean set_int_capacity(int capacity_status){
+    public boolean set_int_Capacity(int capacity_status){
         if(capacity_status <= 0)
             return false;
         this.Capacity=capacity_status;
         return true;
     }
     //Type
-    public List<String> get_list_type() {
-        return this.Type;
-    }
+    public List<String> get_list_Type() {return this.Type;}
     public String get_int_type(int t){
         if (t < 0 || t > this.Type.size() - 1)
             return null;
         return this.Type.get(t);
+    }
+    public boolean add_Type(String new_type) {
+        if (new_type == null)
+            return false;
+        this.Type.add(new_type);
+        return true;
+    }
+    public boolean remove_Type(String old_type) {
+        if (!Type.contains(old_type))
+            return false;
+        this.Type.remove(old_type);
+        return true;
+    }
+    //Location
+    public String get_string_location() {
+        return this.Location;
+    }
+    public boolean set_string_location(String new_location ) {
+        if (new_location == null)
+            return false;
+        this.Location = new_location;
+        return true;
     }
 }
