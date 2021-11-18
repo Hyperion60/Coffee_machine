d957d1a46c114677329d8a01fcf696d78730fec0
Hyperion <mire.maubanc@gmail.com>
2021-11-18 11:32:17 +0100

    Add login function

--- src/connexion/Login.java
+++ src/connexion/Login.java
@@ -1,7 +1,21 @@
 package connexion;
 
+import Structures.Global_lists;
+
 public class Login {
     private User user;
 
+    public Login(Global_lists lists, String name, String password) throws Exception {
+        this.user = null;
+        for (User user: lists.list_user) {
+            if (user.getName().equals(name))
+            {
+                this.user = user;
+                break;
+            }
+        }
+        if (this.user == null || !this.user.checkPassword(password))
+            throw new Exception("User does not exists or invalid password");
+    }
 
 }
