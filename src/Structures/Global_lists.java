33d366d525982ad00fcf54bb2fb16ca8fa3ab15c
Hyperion <mire.maubanc@gmail.com>
2021-11-18 12:33:57 +0100

    Add global class to use their methods

--- src/Structures/Global_lists.java
+++ src/Structures/Global_lists.java
@@ -1,17 +1,29 @@
 package Structures;
 
 import Coffee.Machine;
+import connexion.Login;
+import connexion.Signup;
 import connexion.User;
+import server.ServerThread;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Global_lists {
     public List<User> list_user;
+    public List<ServerThread> list_client;
     public Machine coffee;
+    public Signup signup;
+    public Login login;
+    public int server_port;
 
     public Global_lists() {
         this.list_user = new ArrayList<>();
+        this.list_client = new ArrayList<>();
         this.coffee = new Machine();
+        this.signup = new Signup();
+        this.login = new Login();
+        this.server_port = 867;
     }
 
 }
