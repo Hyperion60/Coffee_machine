31694e9faa6bfff97baa868deaaa0ad3830d533a
Hyperion <mire.maubanc@gmail.com>
2021-11-18 12:37:36 +0100

    Begin method to signup

--- src/connexion/Signup.java
+++ src/connexion/Signup.java
@@ -1,8 +1,13 @@
 package connexion;
 
+import Structures.Global_lists;
+
 public class Signup {
     public void new_account(String name, String password) {
 
+    }
+
+    public void Parser_signup(Global_lists lists, String input) {
     }
 
 }
