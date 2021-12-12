00ce33cb15d3d35f11b59068ffa2d05011f5d56d
Rémi <lasagnes.tomate@gmail.com>
2021-12-12 14:16:04 +0100

    Clean Warning from IOCommand

--- src/Interface/IOCommand.java
+++ src/Interface/IOCommand.java
@@ -2,70 +2,33 @@ package Interface;
 
 import java.io.*;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class IOCommand {
-    private BufferedReader lectureEcran;
-    private PrintStream ecritureEcran;
-    private BufferedReader lectureFichier;
-    private PrintStream ecritureFichier;
-    private Socket socket;
-
-    public IOCommand() {
-        InputStreamReader in = new InputStreamReader(System.in);
-        this.lectureEcran = new BufferedReader(in);
-        this.ecritureEcran = new PrintStream(System.out);
-        this.socket = null;
-        this.lectureFichier = null;
-        this.ecritureFichier = null;
-    }
+    private final Socket socket;
 
     public IOCommand(Socket socket) {
-        InputStreamReader in = new InputStreamReader(System.in);
-        this.lectureEcran = new BufferedReader(in);
-        this.ecritureEcran = new PrintStream(System.out);
         this.socket = socket;
-        this.lectureFichier = null;
-        this.ecritureFichier = null;
     }
 
-    public IOCommand(String path) throws IOException  {
-        InputStreamReader in = new InputStreamReader(System.in);
-        this.lectureEcran = new BufferedReader(in);
-        this.ecritureEcran = new PrintStream(System.out);
-        this.lectureFichier = new BufferedReader(new FileReader(path));
-        this.ecritureFichier = new PrintStream(new FileOutputStream(path, true));
-        this.socket = null;
-    }
-
-    public void ecrireEcran(String texte) {
-        this.ecritureEcran.println(texte);
-    }
-
-    public String lireEcran() throws IOException {
-        return this.lectureEcran.readLine();
-    }
-
-    public int ecrireReseau(String texte) {
+    public int writeNetwork(String text) {
         if (this.socket != null) {
             OutputStream outputStream;
             try {
                 outputStream = this.socket.getOutputStream();
             } catch (IOException e) {
-                outputStream = null;
                 return 1;
             }
             if (outputStream != null) {
                 PrintStream out = new PrintStream(outputStream);
-                out.println(texte);
+                out.println(text);
                 return 0;
             }
         }
         return 1;
     }
 
-    public String lireReseau() {
-        String line = null;
+    public String readNetwork() {
+        String line;
         InputStream inputStream;
         if (this.socket != null) {
             try {
@@ -79,20 +42,12 @@ public class IOCommand {
                 try {
                     line = input.readLine();
                 } catch (IOException exception) {
-                    System.out.println("Echec lecture socket");
+                    System.out.println("Failed to socket read");
                     return null;
                 }
                 return line;
             }
         }
         return null;
-    }
-
-    public void ecrireFichier(String texte) {
-        this.ecritureFichier.println(texte);
-    }
-
-    public String lireFichier() throws IOException {
-        return this.lectureFichier.readLine();
     }
 }
